a4cf45baba36747a17473105e581464c4c1bb6f4
eujap <jose.ap2008@hotmail.com>
2023-05-21 17:18:38 -0400

    uso de classes

--- refprova1.py
+++ refprova1.py
@@ -1,10 +1,18 @@
 class Lancamentos:
 
-    def __init__(self):
-        pass
-        
+    def __init__(self, nome_arquivo):
+        self.nome_arquivo = nome_arquivo
+       
+    
+    def adicionar_lancamento(self, lancamento):
+        with open(self.nome_arquivo, 'a') as arquivo:
+            arquivo.write(str(lancamento) + '\n')
 
-
+    def salvar_lancamentos(self, lancamentos):
+        with open(self.nome_arquivo, 'w') as arquivo:
+            for lancamento in lancamentos:
+                arquivo.write(str(lancamento) + '\n')
+    
     def informar_valor(self, mensagem):
         try:
             valor = float(input(mensagem))
@@ -12,8 +20,8 @@ class Lancamentos:
         except ValueError:
             print('inválido!') 
             return self.informar_valor(mensagem)
-        
-        
+
+
     def informar_inteiro(self, mensagem):
         try:
             valor = int(input(mensagem))
@@ -22,100 +30,157 @@ class Lancamentos:
             print('inválido!') 
             return self.informar_inteiro(mensagem)
         
-
-    def salvar_valor_arquivo(self, linha):
-         with open('salario.txt', 'a+') as arquivo:
-                               
-            arquivo.write(linha)
-            arquivo.write('\n')
-
-   
-    
-    def incluir_salario(self):
-                       
-            ano = self.informar_inteiro('Informe o ano: ')
-            mes =  self.informar_inteiro('Informe o mês: ')
-            salario = self.informar_valor('Informe o salário: ')
-            
-            linha = str(ano) + ',' + str(mes) + ',' + str(salario)
-            self.salvar_valor_arquivo(linha)
-           
-    def alterar_salario(self):
-        ano = self.informar_inteiro('Informe o ano: ')
-        mes =  self.informar_inteiro('Informe o mês: ')
-        salario = self.informar_valor('Informe o novo salário: ')
-        linha_alterada = 0
-        linhas = []
-        with open('salario.txt', 'r') as arquivo:
+    def buscar_lancamento(self, ano, mes):
+        with open(self.nome_arquivo, 'r') as arquivo:
             linhas = arquivo.readlines()
             for linha in linhas:
-                registro = linha.split(',')
-                if (ano == int(registro[0]) and mes == int(registro[1])):
-                    linha_alterada = linhas.index(linha)          
+                registro = linha.strip().split(',')
+                if int(registro[0]) == ano and int(registro[1]) == mes:
+                    return linha
+        return None
+    
+    def alterar_lancamento(self, ano, mes, novo_valor):
+        linhas = []
+        with open(self.nome_arquivo, 'r') as arquivo:
+            linhas = arquivo.readlines()
+            for i, linha in enumerate(linhas):
+                registro = linha.strip().split(',')
+                if int(registro[0]) == ano and int(registro[1]) == mes:
+                    linhas[i] = f"{ano},{mes},{novo_valor}\n"
+                    break
 
-            linhas[linha_alterada] = str(ano) + ',' + str(mes) + ',' + str(salario) + '\n'
-
-        with open('salario.txt', 'w') as arquivo:
+        with open(self.nome_arquivo, 'w') as arquivo:
             arquivo.writelines(linhas)
 
+    def excluir_lancamento(self, ano, mes):
+        linhas = []
+        with open(self.nome_arquivo, 'r') as arquivo:
+            linhas = arquivo.readlines()
 
-    def retornar_saldo(self, ano, mes):
-        with open('salario.txt', 'r') as arquivo:
+        linha_excluida = None
+        for linha in linhas:
+            registro = linha.split(',')
+            if int(ano) == int(registro[0]) and int(mes) == int(registro[1]):
+                linha_excluida = linha
+                break
+
+        if linha_excluida:
+            linhas.remove(linha_excluida)
+
+        with open(self.nome_arquivo, 'w') as arquivo:
+            arquivo.writelines(linhas)  
+
+    def listar_lancamentos(self):
+        lancamentos=[]
+        with open(self.nome_arquivo, 'r') as arquivo:
+            linhas = arquivo.readlines()
+        for linha in linhas:
+            registro = linha.strip().split(',')
+            lancamento = {
+                'ano': int(registro[0]),
+                'mes': int(registro[1]),
+                'valor': float(registro[2])
+            }
+            lancamentos.append(lancamento)
+
+        lancamentos_ordenados = sorted(lancamentos, key=lambda x: (x['ano'], x['mes']))
+        for lancamento in lancamentos_ordenados:
+            print(f"Ano: {lancamento['ano']}, Mês: {lancamento['mes']}, Valor: {lancamento['valor']}")                    
+
+
+
+class Salario(Lancamentos):
+
+    def __init__(self, nome_arquivo):
+        super().__init__(nome_arquivo)
+        self.ano = None
+        self.mes = None
+        self.valor = None
+
+
+    def incluir_salario(self):
+        self.ano = self.informar_inteiro('Informe o ano: ')
+        self.mes = self.informar_inteiro('Informe o mês: ')
+        self.valor = self.informar_valor('Informe o salário: ')
+        self.linha = f"{self.ano},{self.mes},{self.valor}"
+        
+
+    def salvar_salario(self):
+        self.adicionar_lancamento(self.linha)
+
+    def alterar_salario(self):
+        ano = self.informar_inteiro('Informe o ano: ')
+        mes = self.informar_inteiro('Informe o mês: ')
+        novo_valor = self.informar_valor('Informe o novo salário: ')
+        self.alterar_lancamento(ano, mes, novo_valor)        
+
+    def excluir_salario(self):
+        ano = self.informar_inteiro('Informe o ano: ')
+        mes = self.informar_inteiro('Informe o mês: ')
+        self.excluir_lancamento(ano, mes)
+
+    
+class Despesa(Lancamentos):
+
+    def __init__(self, nome_arquivo):
+        super().__init__(nome_arquivo)
+        self.ano = None
+        self.mes = None
+        self.valor = None
+
+    def incluir_despesas(self):
+        self.ano = self.informar_inteiro('Informe o ano: ')
+        self.mes = self.informar_inteiro('Informe o mês: ')
+        self.valor = self.informar_valor('Informe a despesa: ')
+        self.linha = f"{self.ano},{self.mes},{self.valor}"
+
+
+    def salvar_despesa(self):
+        self.adicionar_lancamento(self.linha)
+
+    def alterar_despesa(self):
+        ano = self.informar_inteiro('Informe o ano: ')
+        mes = self.informar_inteiro('Informe o mês: ')
+        novo_valor = self.informar_valor('Informe a nova despesa: ')
+        self.alterar_lancamento(ano, mes, novo_valor)       
+
+    def excluir_despesa(self):
+        ano = self.informar_inteiro('Informe o ano: ')
+        mes = self.informar_inteiro('Informe o mês: ')
+        self.excluir_lancamento(ano, mes)
+
+
+class Resultados(Lancamentos):
+    def __init__(self, nome_arquivo):
+        super().__init__(nome_arquivo)
+        
+
+    def calcular_saldo(self, ano, mes):
+        salario = 0
+        despesa = 0
+
+        
+        with open('salarios.txt', 'r') as arquivo:
             linhas = arquivo.readlines()
             for linha in linhas:
-                registro = linha.split(',')
-                if (ano == int(registro[0]) and mes == int(registro[1])):
-                    return registro[2]
-                
+                registro = linha.strip().split(',')
+                if int(registro[0]) == ano and int(registro[1]) == mes:
+                    salario += float(registro[2])
 
-    def incluir_despesa (self):
-        ano = self.informar_inteiro('Informe o ano: ')
-        mes =  self.informar_inteiro('Informe o mês: ')
-        despesa = self.informar_valor('Informe a despesa: ')
         
-        saldo_mes = float(self.retornar_saldo(ano, mes))
+        with open('despesa.txt', 'r') as arquivo:
+            linhas = arquivo.readlines()
+            for linha in linhas:
+                registro = linha.strip().split(',')
+                if int(registro[0]) == ano and int(registro[1]) == mes:
+                    despesa += float(registro[2])
         
-        if despesa > saldo_mes:
-            print('Saldo insuficiente!')
-        else:   
-            with open('despesa.txt', 'a+') as arquivo:
-                linha = str(ano) + ',' + str(mes) + ',' + str(despesa) + ';'
-                arquivo.write(linha)
-                arquivo.write('\n')        
+        saldo = salario - despesa
+            
+meu_salario = Salario('salarios.txt')
+meu_salario.incluir_salario()
+minha_despesa = Despesa('despesa.txt')
+minha_despesa.incluir_despesas()
 
-class Menu (Lancamentos):
-
-    def __init__(self):
-        pass
-
-
-pedro = Lancamentos()
-#pedro.incluir_salario()
-#pedro.alterar_salario()
-pedro.incluir_despesa()
-
-
-
-
-#    def incluir_despesas():
-
-#    ano = informar_inteiro('Informe o ano: ')
-#    mes =  informar_inteiro('Informe o mês: ')
-#    salario = informar_float('Informe o novo salário: ')
-##    linha_alterada = 0
-##    linhas = []
-##    with open('salario.txt', 'r') as arquivo:
- #       linhas = arquivo.readlines();
- #       for linha in linhas:
- #           registro = linha.split(',')
- #           if (ano == int(registro[0]) and mes == int(registro[1])):
- #               linha_alterada = linhas.index(linha)          
- #       
- #       linhas[linha_alterada] = str(ano) + ',' + str(mes) + ',' + str(salario) + '\n'
- #       
- #   with open('salario.txt', 'w') as arquivo:
- #       arquivo.writelines(linhas)
-#
-#lass Menu:
 
 
